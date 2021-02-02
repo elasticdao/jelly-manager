047347fe15c5361b4a800d6a6ea137800e3ea6f4
Nikoloz <nick@cryptobuilders.io>
2021-02-02 12:05:30 +0200

    WIP: voting mechanism

---
+++ src/Voting/voting.js
@@ -0,0 +1,2 @@
+// Create voting mechanism which will allow, anyone to propose new channel or category. everyone should have a right to propose, accept or decline the proposal.
+// Start writing the scrypt. Should include voting mechanism and basic questions like => 
