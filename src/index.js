9d5813bb6067161371a286216228db4afdbcc51d
Nikoloz <nick@cryptobuilders.io>
2021-02-10 15:57:53 +0200

    fixes eslint issues

--- src/index.js
+++ src/index.js
@@ -102,7 +102,7 @@ class DiscordActions {
       clearTimeout(timeoutPid);
     } else {
       return;
-    }
+    };
 
     await dmChannel.send(`Thanks for verifying that you are not a fellow bot. You are now a member of the ElasticDAO community!`);
 
@@ -121,8 +121,8 @@ class DiscordActions {
 
     const dmChannel = await guildMember.createDM();
     await dmChannel.send(
-      "I'm sorry but I've had to kick you due to a lack of response. " +
-        "Please join ElasticDAO again if you're human.",
+      'I\'m sorry but I\'ve had to kick you due to a lack of response. ' +
+        'Please join ElasticDAO again if you\'re human.',
     );
 
     guildMember.kick(reason);
